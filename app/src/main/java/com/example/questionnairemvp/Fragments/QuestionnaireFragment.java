cdf94a6cfc83c1293ae8c09a49adf99f3db83257
YakushevRoman <pegasus199197@gmail.com>
2019-07-23 16:10:32 +0300

    three commit - fragments

--- app/src/main/java/com/example/questionnairemvp/Fragments/QuestionnaireFragment.java
+++ app/src/main/java/com/example/questionnairemvp/Fragments/QuestionnaireFragment.java
@@ -1,6 +1,21 @@
 package com.example.questionnairemvp.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.questionnairemvp.R;
 
 public class QuestionnaireFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_questionnare, container, false);
+        return view;
+    }
 }
--- app/src/main/java/com/example/questionnairemvp/Fragments/UsersFragment.java
+++ app/src/main/java/com/example/questionnairemvp/Fragments/UsersFragment.java
@@ -1,6 +1,26 @@
 package com.example.questionnairemvp.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.questionnairemvp.R;
 
 public class UsersFragment extends Fragment {
+
+    private RecyclerView recyclerViewUsersFragment;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        recyclerViewUsersFragment = (RecyclerView) view.findViewById(R.id.fragment_users_recycler_view);
+        return view;
+    }
 }
