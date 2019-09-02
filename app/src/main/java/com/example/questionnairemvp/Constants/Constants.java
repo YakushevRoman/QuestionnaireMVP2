7c3c2a9ee5b142c69a483ede131a0103a3b3b2f3
user301 <romanyakushev91@gmail.com>
2019-09-02 15:38:45 +0300

    Revert "commit - new"
    This reverts commit 9f03bb1e
    
    Signed-off-by: user301 <romanyakushev91@gmail.com>

--- app/src/main/java/com/example/questionnairemvp/Constants/Constants.java
+++ app/src/main/java/com/example/questionnairemvp/Constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public class ConstantsGlobal {
         public static final String TAG = "Questionnaire";
-        public static final String TAG5 = "Questionnaire";
+        public static final String TAG6 = "Questionnaire";
     }
 
     public class ConstantsQuestionnaireFragment {
