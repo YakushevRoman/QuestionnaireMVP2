634ec75fe374be0f34ed7abd24193a67915b3c84
user301 <romanyakushev91@gmail.com>
2019-09-02 15:47:38 +0300

    Revert "commit - user fragment delay coment"
    This reverts commit 8626a929
    
    Signed-off-by: user301 <romanyakushev91@gmail.com>

--- app/src/main/java/com/example/questionnairemvp/Constants/Constants.java
+++ app/src/main/java/com/example/questionnairemvp/Constants/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public class ConstantsGlobal {
         public static final String TAG = "Questionnaire";
         public static final String TAG6 = "Questionnaire";
+        public static final String TAG7 = "Questionnaire";
     }
 
     public class ConstantsQuestionnaireFragment {
